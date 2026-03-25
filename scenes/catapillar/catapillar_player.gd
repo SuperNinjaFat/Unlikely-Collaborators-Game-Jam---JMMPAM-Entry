d65fc7698e7ae1d549164e1c594acdffe60a4149
Super Ninja Fat <paulmlindberg@gmail.com>
2026-03-25 19:17:55 -0400

    Make caterpillar player body fly for test purposes

--- scenes/catapillar/catapillar_player.gd
+++ scenes/catapillar/catapillar_player.gd
@@ -5,15 +5,33 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# TODO: Remove. Temporary for testing.
+	gravity_scale = 0
+	frontBody.gravity_scale = 0
+	backBody.gravity_scale = 0
+	axis_lock_linear_z = true
+	axis_lock_angular_z = true
+	axis_lock_angular_x = true
+	axis_lock_angular_y = true
+
 
 # TODO: Use for freezing instead of ".freeze"
 const FROZEN_MASS = 1000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	backBody.freeze = backBody.get_colliding_bodies().size() > 0
-
-	# Positive X impulse to a segment on keypress
-	if Input.is_action_just_pressed("ui_up"):
-		frontBody.apply_central_impulse(Vector3.UP * 4)
+	# backBody.freeze = backBody.get_colliding_bodies().size() > 0
+	# Positive forces to the center segment on keypress
+	# TODO: Remove. Temporary for testing.
+	var direction := Vector3.ZERO
+	if Input.is_action_pressed("ui_up"):
+		direction += Vector3.UP
+	if Input.is_action_pressed("ui_down"):
+		direction += Vector3.DOWN
+	if Input.is_action_pressed("ui_left"):
+		direction += Vector3.LEFT
+	if Input.is_action_pressed("ui_right"):
+		direction += Vector3.RIGHT
+	
+	if direction != Vector3.ZERO:
+		apply_central_impulse(direction.normalized() * 1.0)
