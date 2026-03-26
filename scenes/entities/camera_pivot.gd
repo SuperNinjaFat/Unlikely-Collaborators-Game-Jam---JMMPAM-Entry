d8b9da4b5165120b54fcf1ab59722f92c4d34352
Super Ninja Fat <paulmlindberg@gmail.com>
2026-03-25 23:20:57 -0400

    Refactor sections, add boundaries
    Add commented-out logic to selectively draw sections

--- scenes/entities/camera_pivot.gd
+++ scenes/entities/camera_pivot.gd
@@ -1,14 +1,51 @@
 extends Node3D
 
-@onready var camera_spots = %CameraSpots
-@onready var section_triggers = %SectionTriggers
+# Array of sections, of type Node3D
+@export var sections: Array[Node3D]
+# Track the index of the current section
+var current_section := 0
 
+# On initialization, set up triggers.
 func _ready() -> void:
-    for trigger in section_triggers.get_children():
-        trigger.section_entered.connect(_on_section_entered)
+	for i in sections.size():
+		# Find the trigger of the section, which will be an Area3D.
+		var trigger := _find_trigger(sections[i])
+		if trigger:
+			trigger.section_entered.connect(_on_section_entered.bind(i))
 
+# Upon a section being entered, enable the new section, disable the old one
 func _on_section_entered(index: int) -> void:
-    var spots = camera_spots.get_children()
-    if index < spots.size():
-        var tween = create_tween()
-        tween.tween_property(self , "global_position", spots[index].global_position, 0.5)
+	# Track the previous section so we can disable it after it leaves the camera view
+	var previous_section := current_section
+	current_section = index
+	# Find the CameraSpot of the section, which is a Marker3D.
+	var spot := _find_spot(sections[index])
+	if spot:
+		# Tween to the new CameraSpot.
+		var tween = create_tween()
+		tween.tween_property(self , "global_position", spot.global_position, 0.5)
+		#_set_active_section(index)
+		# Disable the previous section only once the camera stops panning.
+		#tween.finished.connect(func(): _disable_section(previous_section))
+
+
+#func _set_active_section(index: int) -> void:
+	#sections[index].visible = true
+	#sections[index].process_mode = Node.PROCESS_MODE_INHERIT
+
+#func _disable_section(index: int) -> void:
+	#if index != current_section:
+		#sections[index].visible = false
+		#sections[index].process_mode = Node.PROCESS_MODE_DISABLED
+
+func _find_trigger(section: Node3D) -> Area3D:
+	for child in section.get_children():
+		if child is Area3D:
+			return child
+	return null
+
+func _find_spot(section: Node3D) -> Marker3D:
+	for child in section.get_children():
+		if child is Marker3D:
+			return child
+	return null
--- scenes/entities/section_trigger.gd
+++ scenes/entities/section_trigger.gd
@@ -1,21 +1,10 @@
 extends Area3D
 
-@export var section_index: int
+signal section_entered
 
-signal section_entered(index: int)
-
-var triggered := false
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
+    body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node3D) -> void:
-	if not triggered and body.is_in_group("player"):
-		section_entered.emit(section_index)
-		triggered = true
-
-
-# # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta: float) -> void:
-# 	pass
+    if body.is_in_group("player"):
+        section_entered.emit()
