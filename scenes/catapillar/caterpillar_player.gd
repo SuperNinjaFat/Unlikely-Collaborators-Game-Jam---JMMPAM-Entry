f19355d16406ca973d387ebc4c5206937192baa7
turboshook <turboshook@gmail.com>
2026-03-27 11:15:59 -0400

    constrained caterpillar body movement

--- scenes/catapillar/caterpillar_player.gd
+++ scenes/catapillar/caterpillar_player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody3D
 
+const SEGMENT_MAX_DISTANCE_RAMP: float = 2.0
 const SEGMENT_DRAG_DEADZONE: float = 0.25
+const SEGMENT_MIN_TRAVEL_SPEED: float = 0.25
+const SEGMENT_MAX_TRAVEL_SPEED: float = 16.0
 
-@onready var segments: Node3D = $Segments
+@onready var segment_container: Node3D = $SegmentContainer
+@onready var mouse_marker: MeshInstance3D = $MouseMarker
 
-var _segments: Array[CharacterBody3D] = []
+var _all_segments: Array[Node3D] = []
+var _selectable_segments: Array[CharacterBody3D] = []
 var _selected_segment: int = -1
-var _segment_length: float = 1.1
-
-var iterations: int = 4
+var _segment_length: float = 0.0
+var _max_body_length: float = 0.0 # constrains click/drag movement
 
 	### TEMP ###
 var _viewport: Viewport
@@ -16,68 +20,115 @@ var _camera: Camera3D
 
 func _ready() -> void:
 	
+	_segment_length = global_position.distance_to(segment_container.get_child(0).global_position)
+	_max_body_length = global_position.distance_to($SegmentContainer/EndPickableSegment.global_position)
+	
 	# TODO - most likely move to camera manager
 	_viewport = get_viewport()
 	_viewport.physics_object_picking = true
 	_camera = _viewport.get_camera_3d()
 	
-	_segments.append(self)
-	var segment_id: int = 0
-	for segment: CharacterBody3D in segments.get_children():
-		segment_id += 1
-		segment.initialize(segment_id)
-		segment.picked.connect(_on_segment_picked)
-		_segments.append(segment)
+	# TEMP - pending more permanent body scene structure
+	_selectable_segments = [
+		self,
+		$SegmentContainer/MidPickableSegment,
+		$SegmentContainer/EndPickableSegment
+	]
+	$SegmentContainer/MidPickableSegment.initialize(1)
+	$SegmentContainer/EndPickableSegment.initialize(2)
+	$SegmentContainer/EndPickableSegment.picked.connect(_on_segment_picked)
+	
+	_all_segments.append(self)
+	for segment: Node3D in segment_container.get_children():
+		_all_segments.append(segment)
+		segment.set_as_top_level(true)
 
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	if _selected_segment == -1: return
+	
+	# move selected segment toward mouse
 	var projected_global_position: Vector3 = _get_projected_mouse_position()
-	#_segments[_selected_segment].global_position = projected_global_position
+	var opposite_end_segment_position: Vector3 = (
+		_selectable_segments[0].global_position if _selected_segment != 0 else _selectable_segments[2].global_position
+	)
+	var max_travel_position: Vector3 = opposite_end_segment_position + \
+		opposite_end_segment_position.direction_to(projected_global_position) * _max_body_length
+	var min_travel_position: Vector3 = opposite_end_segment_position + \
+		opposite_end_segment_position.direction_to(projected_global_position) * (_max_body_length / 3.0)
+	
+	var travel_target: Vector3 = projected_global_position
+	if opposite_end_segment_position.distance_to(travel_target) > _max_body_length:
+		travel_target = max_travel_position
+	elif opposite_end_segment_position.distance_to(travel_target) < (_max_body_length / 3.0):
+		travel_target = min_travel_position
+	
+	mouse_marker.global_position = travel_target
 	
 	var segment_velocity: Vector3 = Vector3.ZERO
-	if _segments[_selected_segment].global_position.distance_to(projected_global_position) >= 0.25:
+	if _selectable_segments[_selected_segment].global_position.distance_to(travel_target) >= 0.25:
+		var segment_distance_to_mouse: float = _selectable_segments[_selected_segment].global_position.distance_to(travel_target)
 		var distance_ratio: float = clamp(
-			_segments[_selected_segment].global_position.distance_to(projected_global_position) / 2.0, 
+			segment_distance_to_mouse / SEGMENT_MAX_DISTANCE_RAMP, 
 			0.0, 1.0
 		)
-		var velocity_modifier: float = lerp(0.25, 16.0, distance_ratio)
+		var travel_direction: Vector3 = _selectable_segments[_selected_segment].global_position.direction_to(travel_target)
+		var speed: float = lerp(SEGMENT_MIN_TRAVEL_SPEED, SEGMENT_MAX_TRAVEL_SPEED, distance_ratio)
 		segment_velocity = (
-			_segments[_selected_segment].global_position.direction_to(projected_global_position) * velocity_modifier
+			travel_direction * speed
 		)
 	
-	_segments[_selected_segment].set_velocity(segment_velocity)
-	_segments[_selected_segment].move_and_slide()
+	_selectable_segments[_selected_segment].set_velocity(segment_velocity)
+	_selectable_segments[_selected_segment].move_and_slide()
 	
-	_update_segment_positions()
-	_update_segment_rotations()
+	_update_segment_positions(delta)
+	_update_segment_rotations(delta)
+	
 	if Input.is_action_just_released("left_click"): _selected_segment = -1
 
-func _update_segment_positions() -> void:
-	for _i: int in range(iterations):
+func _update_segment_positions(delta: float) -> void:
+	
+	# forward pass
+	if _selected_segment == 0:
 		
-		# Forward pass
-		for i: int in range(_selected_segment + 1, _segments.size()):
-			var prev: CharacterBody3D = _segments[i - 1]
-			var curr: CharacterBody3D = _segments[i]
+		for i: int in range(1, _all_segments.size()):
+			if i == _all_segments.size() - 1: continue
 			
-			var dir: Vector3 = (curr.global_position - prev.global_position).normalized()
-			curr.global_position = prev.global_position + dir * _segment_length
+			var previous: Node3D = _all_segments[i - 1]
+			var current: Node3D = _all_segments[i]
+			
+			var direction: Vector3 = (current.global_position - previous.global_position).normalized()
+			#current.global_position = previous.global_position + direction * _segment_length
+			current.global_position = lerp(
+				current.global_position,
+				previous.global_position + direction * _segment_length, 
+				delta * 16.0
+			)
+		return
+	
+	# backward pass
+	for i: int in range(_all_segments.size() - 2, 0, -1):
+		if i == 0: continue
 		
-		# Backward pass
-		for i: int in range(_selected_segment - 1, -1, -1):
-			var next: CharacterBody3D = _segments[i + 1]
-			var curr: CharacterBody3D = _segments[i]
-			
-			var dir: Vector3 = (curr.global_position - next.global_position).normalized()
-			curr.global_position = next.global_position + dir * _segment_length
+		var next: Node3D = _all_segments[i + 1]
+		var current: Node3D = _all_segments[i]
+		
+		var direction: Vector3 = (current.global_position - next.global_position).normalized()
+		#current.global_position = next.global_position + direction * _segment_length
+		current.global_position = lerp(
+			current.global_position,
+			next.global_position + direction * _segment_length, 
+			delta * 16.0
+		)
 
-func _update_segment_rotations() -> void:
-	for i: int in range(_segments.size() - 1):
-		var curr: CharacterBody3D = _segments[i]
-		var next: CharacterBody3D = _segments[i + 1]
+@warning_ignore("unused_parameter")
+func _update_segment_rotations(delta: float) -> void:
+	for i: int in range(_all_segments.size() - 1):
+		var current: Node3D = _all_segments[i]
+		var next: Node3D = _all_segments[i + 1]
 		
-		curr.look_at(next.global_position, Vector3.UP)
+		# TODO - interpolate rotations
+		current.look_at(next.global_position, Vector3.UP)
 
 @warning_ignore("unused_parameter")
 func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
@@ -85,7 +136,7 @@ func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, 
 	_on_segment_picked(0)
 
 func _on_segment_picked(segment_id: int) -> void:
-	segment_id = clamp(segment_id, 0, _segments.size() - 1)
+	segment_id = clamp(segment_id, 0, _selectable_segments.size() - 1)
 	_selected_segment = segment_id
 
 func _get_projected_mouse_position() -> Vector3:
